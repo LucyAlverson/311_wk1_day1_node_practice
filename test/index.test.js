89d799b33e3e9de1f53bf59758daa841e599d04d
Lucy Alverson <lucyalverson@icloud.com>
2021-07-03 12:27:04 -0500

    bottom half questions

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,40 @@
 const { numbers, strings, dates } = require('../src')
 
-describe('Numbers module', () => {
-
+  function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
+}
   test('Check if a given number is even or odd', () => {
     const isEven = numbers.isEven(29384)
     expect(isEven).toBe(true)
   })
 
+var foo = '1';
+typeof (foo - 0); // => 'number'
+
+var a = '1',
+    b = '2',
+    c = '3',
+    d = '4',
+    e = '5',
+    f = '6';
+console.log((a - 0) + (b - 0)); // 21
+
   test('Add multiple numbers', () => {
     const sum = numbers.sum([1, 2, 3, 4, 5, 6])
     expect(sum).toEqual(21)
   })
+
+Input : arr[] = 1, 3, 6, 2, 9 
+            x = 14
+Output : [1, 1, 1, 1]
+         [1, 1, 3]
+         [1, 6]
+         [2, 2]
+         [9] 
+
 
   test('Determine if any combination equals a given sum', () => {
     const exists = numbers.comboSum([1, 3, 6, 2, 9], 14)
